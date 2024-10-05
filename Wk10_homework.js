5b164d47ebee3c5ade36d1b5803c3eaef2cb4ff3
timothy <tvog0001@student.monash.edu>
2024-10-05 11:24:49 +1000

    update

--- Wk10_homework.js
+++ Wk10_homework.js
@@ -1,5 +1,5 @@
-var vg_1 = "Week_9_Homework.json"; 
-vegaEmbed("#Aus_map", vg_1).then(function(result) { 
+var vg_1 = "Week_10_Homework.json"; 
+vegaEmbed("#Aus_map2", vg_1).then(function(result) { 
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
