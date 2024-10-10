3553f87a540a6d35c6ab088464ad7b41ea415fe9
timothy <tvog0001@student.monash.edu>
2024-10-10 15:45:27 +1100

    Update HTML

---
+++ data_vis.js
@@ -0,0 +1,25 @@
+var vg_3 = "Visualisations/AOS_Age.json"; 
+vegaEmbed("#AOS_Age", vg_3).then(function(result) { 
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_4 = "Visualisations/AOS_Gender.json"; 
+vegaEmbed("#AOS_Gender", vg_4).then(function(result) { 
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_5 = "Visualisations/PercentageAOS_State.json"; 
+vegaEmbed("#PAOS_State", vg_5).then(function(result) { 
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_6 = "Visualisations/PercetageAge_HighSchool.json"; 
+vegaEmbed("#PAGE_HS", vg_6).then(function(result) { 
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_7 = "Week_10_Homework.json"; 
+vegaEmbed("#Aus_map2", vg_7).then(function(result) { 
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
