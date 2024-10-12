f45801dc7625889531c6996f521cb8dbb39c5e56
timothy <tvog0001@student.monash.edu>
2024-10-12 16:10:57 +1100

    Huge update

--- data_vis.js
+++ data_vis.js
@@ -1,25 +1,25 @@
 var vg_3 = "Visualisations/AOS_Age.json"; 
-vegaEmbed("#AOS_Age", vg_3).then(function(result) { 
+vegaEmbed("#AOS_Age", vg_3, {actions: false}).then(function(result) { 
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 var vg_4 = "Visualisations/AOS_Gender.json"; 
-vegaEmbed("#AOS_Gender", vg_4).then(function(result) { 
+vegaEmbed("#AOS_Gender", vg_4, {actions: false}).then(function(result) { 
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 var vg_5 = "Visualisations/PercentageAOS_State.json"; 
-vegaEmbed("#PAOS_State", vg_5).then(function(result) { 
+vegaEmbed("#PAOS_State", vg_5, {actions: false}).then(function(result) { 
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 var vg_6 = "Visualisations/PercetageAge_HighSchool.json"; 
-vegaEmbed("#PAGE_HS", vg_6).then(function(result) { 
+vegaEmbed("#PAGE_HS", vg_6, {actions: false}).then(function(result) { 
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 var vg_7 = "Visualisations/Bachelors_State.json"; 
-vegaEmbed("#Aus_map2", vg_7).then(function(result) { 
+vegaEmbed("#Aus_map2", vg_7, {actions: false}).then(function(result) { 
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
